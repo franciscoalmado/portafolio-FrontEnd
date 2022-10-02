594e54c55231021cb9ead4092bfff406b68d8cfd
franciscoalmado <falmado@gmail.com>
2022-10-01 22:54:30 -0300

    Portfolio en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SocialNetworksComponent } from './components/social-networks/social-networks.component';
+import { LoginButtonComponent } from './components/login-button/login-button.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { LanguageComponent } from './components/language/language.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    SocialNetworksComponent,
+    LoginButtonComponent,
+    BannerComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    LanguageComponent,
+    ProjectsComponent,
   ],
   imports: [
     BrowserModule,
